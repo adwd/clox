873a25fcfff6ed79cb7fc834eaeb1c6901e638c5
adwd <ecdt.mn@gmail.com>
2023-09-30 22:44:28 +0900

    compile expression

--- vm.c
+++ vm.c
@@ -51,23 +51,23 @@ static InterpretResult run() {
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
-        case OP_CONSTANT: {
-            Value constant = READ_CONSTANT();
-            push(constant);
-            printValue(constant);
-            printf("\n");
-            break;
-        }
-        case OP_ADD:      BINARY_OP(+); break;
-        case OP_SUBTRACT: BINARY_OP(-); break;
-        case OP_MULTIPLY: BINARY_OP(*); break;
-        case OP_DIVIDE:   BINARY_OP(/ ); break;
-        case OP_NEGATE: push(-pop()); break;
-        case OP_RETURN: {
-            printValue(pop());
-            printf("\n");
-            return INTERPRET_OK;
-        }
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                push(constant);
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/ ); break;
+            case OP_NEGATE: push(-pop()); break;
+            case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+            }
         }
     }
 
@@ -77,6 +77,19 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(const char* source) {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
